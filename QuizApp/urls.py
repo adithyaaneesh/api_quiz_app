81482bcef9f73022cbf3dda84cdf61140f871b21
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-11-24 20:34:39 +0530

    add get_quiz function

--- QuizApp/urls.py
+++ QuizApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    # path('',views.sample),
+    path('api/get_quiz',views.get_quiz),
 ]
--- QuizApp/views.py
+++ QuizApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import QuizSerializer 
+from .models import Quiz, Option
 # Create your views here.
+
+# get quiz
+@api_view(['GET'])
+def get_quiz(request):
+    quiz_questions = Quiz.objects.all()
+    serializer = QuizSerializer(quiz_questions, many=True)
+    return Response(serializer.data)
+
